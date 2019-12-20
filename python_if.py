673067b266c89f6c27eae5f59abbb6beb6b8cffb
어지훈 <hilton3180@gmail.com>
2019-12-20 15:15:22 +0900

    python_if

---
+++ python_if.py
@@ -0,0 +1,21 @@
+##if 조건문
+
+#and, or, not
+x and y#x&&y
+x or y#x||y
+not x#!x
+
+#안에 있는가 in
+x in list
+x in tuple
+x in String
+x not in ...
+
+#조건 만족시 지나가는 것 pass
+if 'money' in pocket:
+    pass
+
+#else if== elif
+
+#조건부 표현식
+message="success" if score >=60 else "failure"
